cb33658227aa51c6c4452e69d8bf41267967e045
Борис Васильев <1024sparrow@gmail.com>
2018-03-01 10:03:42 +0300

    Добавлены пакеты на инициальную установку: qtcreator и mplayer

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-tmp=$(pwd)
+#apt install vim synaptic git openbox wmctrl
+apt install qtcreator mplayer
 
-cp clone_terminal /usr/local/bin/
-cd /usr/local/bin && ln -s ${tmp}/projects/80215_desktop_indicator/80215_desktop_indicator ./
+#tmp=$(pwd)
+
+#cp clone_terminal /usr/local/bin/
+#cd /usr/local/bin && ln -s ${tmp}/projects/80215_desktop_indicator/80215_desktop_indicator ./
