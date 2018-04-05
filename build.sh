6ae12b990ce32ef5bda6941be73ab2a2ec928868
1024sparrow <1024sparrow@gmail.com>
2018-04-05 10:58:33 +0300

    123

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-tmp=$(pwd)
+DEST_LIST=".bashrc .gconf .vim .vimrc"
+for i in $DEST_LIST
+do
+    rm -rf ~/$i
+    ln -s "$(pwd)/$i" ~/$i
+done
 
-cp -f .bashrc ~/
-cp -rf .gconf ~/
-cp -rf .vim ~/
-cp -f .vimrc ~/
-
-mkdir openbox
-cd openbox
-cp config_openbox/* openbox/
-mv openbox ~/.config/
+ln -s "$(pwd)"/config_openbox ~/.config/openbox
 
 bash generates_desktop_home_files_for_desktop_indicator.sh
 
--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#apt install vim synaptic git openbox wmctrl
+apt install -y mc vim synaptic git openbox wmctrl
+apt install -y python python-pip python-virtualenv
+apt install -y qt5-qmake qt5-doc qt5-default qtmultimedia5-dev
 apt install -y qtcreator mplayer gparted cmake
-apt install -y libc6:i386
+apt install -y libc6:i386 npm
 
 #install nodeJS
 apt install curl
@@ -13,5 +15,6 @@ apt-get install -y build-essential
 
 #tmp=$(pwd)
 
-#cp clone_terminal /usr/local/bin/
+cp clone_terminal /usr/local/bin/
 #cd /usr/local/bin && ln -s ${tmp}/projects/80215_desktop_indicator/80215_desktop_indicator ./
+ln -s "$(pwd)/projects/80215_desktop_indicator/80215_desktop_indicator" /usr/local/bin/80215_desktop_indicator
