14befde94c6a93af45116bc7f2a62aec2b0e3e8f
1024sparrow <1024sparrow@gmail.com>
2018-04-05 16:10:24 +0300

    123

--- install.sh
+++ install.sh
@@ -2,19 +2,22 @@
 
 apt install -y mc vim synaptic git openbox wmctrl
 apt install -y python python-pip python-virtualenv
-apt install -y qt5-qmake qt5-doc qt5-default qtmultimedia5-dev
-apt install -y qtcreator mplayer gparted cmake
+apt install -y qtcreator qt5-qmake qt5-doc qt5-default qtmultimedia5-dev
+apt install -y mplayer gparted cmake
 apt install -y libc6:i386 npm
 
-#install nodeJS
+# install nodeJS
 apt install curl
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 apt-get install -y build-essential
 
-
-#tmp=$(pwd)
+# install compiler
+git clone https://github.com/1024sparrow/compiler.git
+cd compiler
+npm install
+sudo ln -s $(pwd)/compile.js /usr/local/bin/compile
+cd ..
 
 cp clone_terminal /usr/local/bin/
-#cd /usr/local/bin && ln -s ${tmp}/projects/80215_desktop_indicator/80215_desktop_indicator ./
 ln -s "$(pwd)/projects/80215_desktop_indicator/80215_desktop_indicator" /usr/local/bin/80215_desktop_indicator
