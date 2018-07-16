afc979a5bb9a01ffb133aa84dfabc639f5b682e0
1024sparrow <1024sparrow@gmail.com>
2018-07-16 14:49:12 +0300

    fixed. for ubuntu-18.04(LTS)

--- install.sh
+++ install.sh
@@ -17,7 +17,8 @@ function install()
 	for i in $1
 	do
 		echo "Устанавливается пакет $i"
-		apt-cache show $i > /dev/null && apt-get install > /dev/null $i || dep_exit "Пакет $i не найден в репозитории"
+		apt-cache show $i > /dev/null && apt-get install -y $i > /dev/null || dep_exit "Пакет $i не найден в репозитории"
+		#apt-cache show $i && apt-get install $i || dep_exit "Пакет $i не найден в репозитории"
 	done
 }
 
@@ -42,7 +43,7 @@ fi
 install "mc vim synaptic git"
 
 if [ -z $INSTALL_OPENBOX ];then
-    install openbox wmctrl kolourpaint4
+    install "openbox wmctrl kolourpaint4"
     cp -u clone_terminal /usr/local/bin/
     rm /usr/local/bin/80215_desktop_indicator
     ln -s "$(pwd)/projects/80215_desktop_indicator/80215_desktop_indicator" /usr/local/bin/80215_desktop_indicator
@@ -51,12 +52,13 @@ fi
 install "python python-pip python-virtualenv"
 install "python3 python3-pip python3-virtualenv"
 install "qtcreator qt5-qmake qt5-doc qt5-default qtmultimedia5-dev"
-install "qml qmlscene libqt5qml-graphicaleffects"
-install "mplayer gparted cmake"
+#install "qml qmlscene libqt5qml-graphicaleffects"
+install "qml qmlscene"
+install "mplayer gparted cmake feh gimp"
 install "kdelibs5-plugins kdelibs5-data oxygen-icon-theme kolourpaint4"
 
 if [ -z $INSTALL_TRALIVA_COMPILE ];then
-    install libc6:i386 npm
+    install "libc6:i386 npm"
     # install nodeJS
     install curl
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - || dep_exit "fail..."
